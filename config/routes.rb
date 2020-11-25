f9821540577c9316ec67cb5a07772ee6afc40939
Marcos Costa <marcos.liborio@gmail.com>
2020-11-25 12:26:18 -0300

    implementada rota ask

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get '/', to: 'pages#home'
+  # root to: 'pages#home'
+
+  # verb '/path', to: 'controller#action'
+  get '/ask', to: 'questions#ask'
 end
+
+# FOLLOW THE FLOW
+# SEGUE A BATERIA
+# routes -> controller -> view
