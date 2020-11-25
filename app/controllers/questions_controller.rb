418f32faa0d90dd499902e41b051d1380a99ba5b
Marcos Costa <marcos.liborio@gmail.com>
2020-11-25 15:37:15 -0300

    implementado Stupid Coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,11 @@
 class QuestionsController < ApplicationController
-
   def ask
   end
 
+  def answer
+    @user_input = params[:answer]
+  end
+
+  def home
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # get '/', to: 'pages#home'
-  # root to: 'pages#home'
+  root to: 'questions#home'
 
   # verb '/path', to: 'controller#action'
   get '/ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
 end
 
 # FOLLOW THE FLOW
